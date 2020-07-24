3cd94541cf9941d4a49eb3207920b09fb3859582
TateoKohara <68531009+TateoKohara@users.noreply.github.com>
2020-07-24 17:36:24 +0900

    チャットルーム作成機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_07_24_045324) do
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "rname", null: false
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
